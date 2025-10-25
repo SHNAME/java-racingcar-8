dc13b1380dd0ed0d5d4ffdd0223015138f7cea6d
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-25 10:44:57 +0900

    refactor(NameValidator): 중첩 깊이를 줄이기 위해 검증 로직을 별도의 메서드로 분리

--- src/main/java/racingcar/validator/NameValidator.java
+++ src/main/java/racingcar/validator/NameValidator.java
@@ -5,10 +5,15 @@ import java.util.List;
 public final class NameValidator {
     public static void validateCarLength(List<String> names) {
         for (String name : names) {
-            if (name.isEmpty() || name.length() > 5) {
-                throw new IllegalArgumentException("1자 이상 5글자 이하로 자동차 이름을 입력해주세요");
-            }
+            validateNameLength(name);
         }
     }
+
+    private static void validateNameLength(String name) {
+        if (name.isEmpty() || name.length() > 5) {
+            throw new IllegalArgumentException("1자 이상 5글자 이하로 자동차 이름을 입력해주세요");
+        }
+    }
+
 
 }
