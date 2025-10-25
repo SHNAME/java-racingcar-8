1db4059067387dc9d0bdec41e961cea8cf2a1578
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-25 10:50:52 +0900

    feat(NameValidator): 자동차 이름이 알파벳으로만 이루어져 있는지 검증

--- src/main/java/racingcar/validator/NameValidator.java
+++ src/main/java/racingcar/validator/NameValidator.java
@@ -9,6 +9,18 @@ public final class NameValidator {
         }
     }
 
+    public static void validateAlphabet(List<String> names) {
+        for (String name : names) {
+            isAlphabet(name);
+        }
+    }
+
+    private static void isAlphabet(String name) {
+        if (!name.matches("^[a-z|A-Z]+$")) {
+            throw new IllegalArgumentException("자동차 이름은 소문자와 대문자로 구성해주세요");
+        }
+    }
+
     private static void validateNameLength(String name) {
         if (name.isEmpty() || name.length() > 5) {
             throw new IllegalArgumentException("1자 이상 5글자 이하로 자동차 이름을 입력해주세요");
