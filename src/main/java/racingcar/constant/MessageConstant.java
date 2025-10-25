0656b95dd130caba85681ffc0e36de29ff391403
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-26 00:19:37 +0900

    feat(MessageConstant): 총 횟수 요청 문구 출력 기능 구현
    전진을 시도하는 총 횟수를 요청하는 문구를 상수로 선언

--- src/main/java/racingcar/constant/MessageConstant.java
+++ src/main/java/racingcar/constant/MessageConstant.java
@@ -1,7 +1,8 @@
 package racingcar.constant;
 
 public enum MessageConstant {
-    AUTOMOBILE_INPUT_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    AUTOMOBILE_INPUT_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    TOTAL_COUNT_INPUT_MESSAGE("시도할 횟수는 몇 회인가요?");
 
     private final String message;
 
