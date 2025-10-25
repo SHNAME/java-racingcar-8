958af5283b51969f79f90702ab52ed93b58b4a4b
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-25 11:47:11 +0900

    feat(InputValidator): 쉼표가 비정상적으로 입력되는 경우를 검증한다.

---
+++ src/main/java/racingcar/utils/Parser.java
@@ -0,0 +1,7 @@
+package racingcar.utils;
+
+public final class Parser {
+    public static String[] splitInput(String userInput) {
+        return userInput.split(",");
+    }
+}
--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,5 +1,7 @@
 package racingcar.validator;
 
+import racingcar.utils.Parser;
+
 public final class InputValidator {
 
     public static void validateInputIsEmpty(String userInput) {
@@ -7,4 +9,19 @@ public final class InputValidator {
             throw new IllegalArgumentException("빈 문자열을 입력했습니다. 다시 입력해 주세요");
         }
     }
+
+    public static void validateSeparatorInInput(String userInput) {
+        String[] names = Parser.splitInput(userInput);
+        for (String name : names) {
+            checkNameIsNotBlank(name);
+        }
+    }
+
+    private static void checkNameIsNotBlank(String carName) {
+        if (carName.isBlank()) {
+            throw new IllegalArgumentException("입력값에서 쉼표로 구분된 각 항목은 유효한 자동차 이름이어야 합니다.");
+        }
+    }
+
+
 }
