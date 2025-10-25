bb8a61ea7ef06b14a96fde41a4c89da09faeadae
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-25 10:31:51 +0900

    feat(NameValidator): 입력된 자동차 이름의 길이를 검증

---
+++ src/main/java/racingcar/validator/NameValidator.java
@@ -0,0 +1,14 @@
+package racingcar.validator;
+
+import java.util.List;
+
+public final class NameValidator {
+    public static void validateCarLength(List<String> names) {
+        for (String name : names) {
+            if (name.isEmpty() || name.length() > 5) {
+                throw new IllegalArgumentException("1자 이상 5글자 이하로 자동차 이름을 입력해주세요");
+            }
+        }
+    }
+
+}
