b6fee90b2176a23d65269917cc1b251b6338870b
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-26 00:11:20 +0900

    refactor(Parser): 클래스 내부에서 사용되는 구분자 기호를 상수로 변경

--- src/main/java/racingcar/utils/Parser.java
+++ src/main/java/racingcar/utils/Parser.java
@@ -1,8 +1,10 @@
 package racingcar.utils;
 
+import racingcar.constant.SymbolConstant;
+
 public final class Parser {
     public static String[] splitInput(String userInput) {
-        return userInput.split(",");
+        return userInput.split(SymbolConstant.NAME.getSymbol());
     }
 
 }
