ebe1891f965f55030008e2d0a7a9f179bae916f6
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-25 23:22:15 +0900

    test(NameTest): Car과 Cars 도메인 검증 테스트 구현
    - Car 도메인: 자동차 이름 길이(5글자 이상) 및 유효하지 않는 문자(영문 이외 문자) 테스트
    - Cars 도메인: 자동차 이름 중복 여부 테스트

--- src/test/java/racingcar/NameTest.java
+++ src/test/java/racingcar/NameTest.java
@@ -1,9 +1,13 @@
 package racingcar;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
 import racingcar.validator.InputValidator;
+import racingcar.validator.NameValidator;
 
 public class NameTest {
 
@@ -27,6 +31,45 @@ public class NameTest {
         String formatErrorInput = "car1,,car2";
         Assertions.assertThatThrownBy(() -> InputValidator.validateSeparatorInInput(formatErrorInput))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void CAR_도메인_단에서_검증_성공() {
+        String successInput = "Pobi";
+        Car.of(successInput);
+    }
+
+    @Test
+    void CAR_도메인_단에서_검증_실패_자동차_이름_5글자_이상() {
+        String successInput = "pobiTest";
+        Assertions.assertThatThrownBy(() -> Car.of(successInput)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1자 이상 5글자 이하로 자동차 이름을 입력해주세요");
+    }
+
+    @Test
+    void CAR_도메인_단에서_검증_실패_자동차_이름에_숫자_포함() {
+        String successInput = "Pobi3";
+        Assertions.assertThatThrownBy(() -> Car.of(successInput)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 소문자와 대문자로 구성해주세요");
+    }
+
+    @Test
+    void CARS_단에서_검증_성공() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(Car.of("pobi"));
+        cars.add(Car.of("ted"));
+        cars.add(Car.of("jyp"));
+        NameValidator.validateCarsFormat(cars);
+    }
+
+    @Test
+    void CARS_단에서_검증_실패_중복된_이름_포함() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(Car.of("pobi"));
+        cars.add(Car.of("pobi"));
+        cars.add(Car.of("jyp"));
+        Assertions.assertThatThrownBy(() -> NameValidator.validateCarsFormat(cars))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("자동차 이름은 중복될 수 없습니다.");
     }
 
 
