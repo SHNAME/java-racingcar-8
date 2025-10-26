80d58580b134a976ef7b8d7d152a483dce55f942
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-26 15:05:58 +0900

    feat(OutputView): 뷰에서 우승자를 출력하는 함수 구현

--- src/main/java/racingcar/constant/MessageConstant.java
+++ src/main/java/racingcar/constant/MessageConstant.java
@@ -3,7 +3,8 @@ package racingcar.constant;
 public enum MessageConstant {
     AUTOMOBILE_INPUT_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     TOTAL_COUNT_INPUT_MESSAGE("시도할 횟수는 몇 회인가요?"),
-    EXECUTION_RESULT_MESSAGE("실행 결과");
+    EXECUTION_RESULT_MESSAGE("실행 결과"),
+    WINNER_RESULT_MESSAGE("최종 우승자");
 
     private final String message;
 
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,11 +1,18 @@
 package racingcar.view;
 
+import java.util.List;
 import racingcar.constant.MessageConstant;
+import racingcar.constant.SymbolConstant;
 
 public class OutputView {
 
     public void printMessage(MessageConstant message) {
         System.out.println(message.getMessage());
+    }
+
+    public void printWinner(MessageConstant message, List<String> winnerList) {
+        String winnerName = String.join(SymbolConstant.NAME.getSymbol(), winnerList);
+        System.out.println(message.getMessage() + " : " + winnerName);
     }
 
 }
